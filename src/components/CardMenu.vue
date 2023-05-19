d9af5de6043afb9a52de06da1e1d340a8f7eb721
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-05-19 18:28:52 +0700

    .

---
+++ src/components/CardMenu.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>{{ menu.nama }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardMenu',
+  props: ['menu'],
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,56 @@
 <template>
   <div class="home container">
     <Hero />
+
     <div class="row my-5">
       <div class="col">
         <h3>Best <strong>Menu</strong></h3>
       </div>
       <div class="col">
         <router-link
-          to="/"
+          to="/menu"
           class="btn btn-success text-white float-end fw-semibold"
           ><i class="bi bi-eye"></i> See All Menus</router-link
         >
+      </div>
+    </div>
+
+    <div class="row mt-5">
+      <div class="col-md-4" v-for="menu in menus" :key="menu.id">
+        <CardMenu :menu="menu" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Axios from 'axios';
 import Hero from '@/components/Hero.vue';
+import CardMenu from '@/components/CardMenu.vue';
 
 export default {
   name: 'HomeViews',
   components: {
     Hero,
+    CardMenu,
+  },
+
+  data() {
+    return {
+      menus: [],
+    };
+  },
+
+  methods: {
+    setMenus(data) {
+      this.menus = data;
+    },
+  },
+
+  mounted() {
+    Axios.get('http://localhost:3000/best-menus')
+      .then((response) => this.setMenus(response.data))
+      .catch((error) => console.log(error));
   },
 };
 </script>

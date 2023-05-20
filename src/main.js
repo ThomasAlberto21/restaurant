73bcea9b959a1a9cca2c44ae0b7f2264db984c66
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-05-20 10:59:55 +0700

    .

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import './assets/main.css';
 import App from './App.vue';
 import router from './router';
+import Toast from 'vue-toastification';
 import { createApp } from 'vue';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'vue-toastification/dist/index.css';
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(router, Toast).mount('#app');
